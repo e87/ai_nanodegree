a859c6cbf395e0d31943866f3abdc1c775fb8c26
Juan Sanchez <jesv8@hotmail.com>
2018-10-10 23:53:55 -0500

    Added the Scripting Directory and some scripts

---
+++ intro_to_python/scripting/user_input.py
@@ -0,0 +1,14 @@
+# Taking some input from the user
+
+name = input("Please enter your name: ").split(",")
+print("Your name is {}".format(name))
+
+# You can accept numbers as input and cast them like this:
+
+number = int(float(int(input("Please enter a number: "))))
+print("Hello " * number)
+
+# Using Eval. It allows user to enter python expresions as inputself.
+
+result = eval(input("Enter an expresion for me: "))
+print(result)
